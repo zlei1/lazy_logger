e13db12c5a2a12c808eff8c30f9d371c830c745b
zlei1 <zhenglei123a@gmail.com>
2019-03-01 17:26:29 +0800

    add test

--- test/lazy_logger_test.rb
+++ test/lazy_logger_test.rb
@@ -1,11 +1,20 @@
-require "test_helper"
+require_relative 'test_helper'
+
+require 'fileutils'
 
 class LazyLoggerTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::LazyLogger::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_log
+    LazyLogger.test.info "hello world"
+
+    lines = IO.readlines("test.log")
+
+    assert_equal "hello world", lines.last.strip
+
+    FileUtils.rm_f("test.log")
   end
+
 end
